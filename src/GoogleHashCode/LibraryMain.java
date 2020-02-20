fe5149ca214a7f9328362a40e9c7fed58cd03ed8
Johnny Pham <jkvp@uw.edu>
2020-02-20 12:57:25 -0800

    inputs

--- src/GoogleHashCode/LibraryMain.java
+++ src/GoogleHashCode/LibraryMain.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 
 public class LibraryMain {
     public static void main(String[] args) throws FileNotFoundException {
-        File inputFile = new File("b_read_on.txt");
+        File inputFile = new File("a_example.txt");
         Scanner input = new Scanner(inputFile);
         String firstLine = input.nextLine();
         String[] arr = firstLine.split(" ");

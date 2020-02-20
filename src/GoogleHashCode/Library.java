6f3b1a0805af1ac08546c830d5b7055ce11cbd70
estefanofc <estefano_fc@hotmail.com>
2020-02-20 11:39:17 -0800

    added book and library class

--- src/GoogleHashCode/Library.java
+++ src/GoogleHashCode/Library.java
@@ -1,25 +1,31 @@
 package GoogleHashCode;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Library {
-    private Book[] bookList;
+    private List<Book> bookList;
     private int signUpTime;
     private int scannedBooks;
     private int possiblePoints;
+    private boolean visited;
+    private int booksSize;
+    private PriorityQueue<Book> pq = new PriorityQueue<>();
 
-    public Library(Book[] bookList, int signUpTime, int scannedBooks) {
-        Arrays.sort(bookList);
-        this.bookList = bookList;
+    public Library(int signUpTime, int scannedBooks) {
+        bookList = new ArrayList<>();
         this.signUpTime = signUpTime;
         this.scannedBooks = scannedBooks;
         possiblePoints = 0;
+        visited = false;
     }
 
-    public Book[] getBookList() {
+    public List<Book> getBookList() {
         return bookList;
     }
 
-    public void setBookList(Book[] bookList) {
+    public void setBookList(List<Book> bookList) {
         this.bookList = bookList;
     }
 
@@ -46,4 +52,17 @@ public class Library {
     public void setPossiblePoints(int possiblePoints) {
         this.possiblePoints = possiblePoints;
     }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean addBook(Book book) {
+        return bookList.add(book);
+    }
+
 }
